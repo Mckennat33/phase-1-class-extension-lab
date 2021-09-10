0d6ad5775b15be1bfb11316578bc0e45f8daec1c
Thomas <thomasmckenna12gmail.com>
2021-09-09 20:51:37 -0400

    commit tree 751c724496eaddba878add603ff8df1f5c03e38e

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+class Polygon {
+   constructor(array) {
+    this.array = array 
+   }
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+     return this.array.length * this.array[0]
+    }
+}
+
+class Triangle extends Polygon {
+    constructor(array) {
+       super(array)     
+    }
+
+    get isValid() {
+        for (let i = 1; i < this.array.length; i++) {
+            if (this.array[i] !== this.array[0]) {
+              return false;
+            }
+          }
+          return true;
+        }
+
+    get perimeter() {
+        return this.array.length * this.array[0]
+    }
+
+}
+
+
+class Square extends Polygon {
+    constructor(array){
+    super()
+    }
+}
+
+// class Triangle extends Polygon {
+//     constructor(array) {
+//        super(array)     
+//     }
+// 
+//     get isValid() {
+//       if (this.array.length === 3){
+//           return true 
+//       } 
+//     }
+// }
